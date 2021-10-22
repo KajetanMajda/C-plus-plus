3ff63f3bccd82153d1577f5b83de095fdc9dbd27
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2021-10-22 18:31:00 +0200

    Add files via upload

---
+++ s03-echo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+auto main(int argc, char*argv[]) -> int
+{
+  int x = 1;
+  while (x <= argc)
+  { 
+    auto var1 = std::string{argv[1]};
+    auto var2 = std::string{argv[2]};
+    if (var1 == "-n") 
+    {
+      std::cout << argv[x+1] << " ";
+    }
+    else if (var1 == "-l") std::cout << argv[x+1] << "\n";
+    else if (var1 == "-r") 
+    {
+      for (int x=(argc-1); x>=2; x--) std::cout << argv[x] << " ";
+      return 0;
+    }
+    else std::cout << argv[x] << " ";
+    x++;
+  };
+  std::cout << "test";
+  return 0;
+}
